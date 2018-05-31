05c564b59fb14377dcc9fbed6a894210a47f5b0a
KentaKawamata <s16c1044sc@s.chibakoudai.jp>
2018-05-30 22:28:21 -0700

    success calculate

--- dp003.py
+++ dp003.py
@@ -1,39 +1,53 @@
 import numpy as np
 import os
 
-def main():
+def readfiles(filename, dirname):
 
-  filename = "city011_001.txt"
-  dirname = "./datasets/city011/"
   name = os.path.join(dirname, filename)
 
   with open(name, "r") as fp:
     city = fp.readlines()
 
-
-  
   datas = []
   for i, line in enumerate(city):
     if i==0:
       sireal = str(line)
       sireal = sireal.replace('\n', '')
-    if i==1:
+    elif i==1:
       city_name = str(line)
       city_name = city_name.replace('\n', '')
-    if i==2:
+    elif i==2:
       file_num = int(line)
     else:
+      line = line.replace('\n', '')
+      line = line.split()
       datas.append(line)
 
-  data=" "
-  for i in datas:
-    data += i
-
-  data = data.replace('\n', '')
+  data = np.array(datas, dtype=float)
+  data = np.reshape(data, (data.size))
   print(sireal)
   print(city_name)
   print(file_num)
+  print(data.shape)
   print(data)
+
+  #return sireal, city_name, file_num, data
+  return sireal, city_name, data.size, data
+
+def main():  
+
+  sireal1, word1, num1, data1 = readfiles("city011_001.txt", "./datasets/city011/")
+  sireal2, word2, num2, data2 = readfiles("city012_001.txt", "./datasets/city012/")
+
+  matrix = np.zeros((num2, num1))
+  
+  print(data1.shape)
+
+  for y in range(num2):
+    for x in range(num1):
+      matrix[y,x] = data2[y] - data1[x]
+  
+  print(matrix)
 
 if __name__ == "__main__":
 
